722e942c35ed712b9ccdec956fcd5e7b1e3a57f3
Preston Elliott <preston.elliott24@gmail.com>
2019-04-05 15:49:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,9 +25,7 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest["oil paintings"]
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -37,13 +35,15 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4
 
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
